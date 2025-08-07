f6d7bd32f5a658ab3ba05e13fb9b5b1c0b2d0729
Tejas Singh <tejassingh649@rediffmail.com>
2025-08-07 11:49:45 +0000

    [SQUASHED] Set default game props
    - [tejas101k] - Originally set default game props.
    - [donjohanliebert] - Moved them to product.prop for better structure.
    
    Change-Id: I91ce80320642aa35e0adf47aa90ee5545f7d9d29
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config.mk
+++ config.mk
@@ -4,3 +4,6 @@ include vendor/spoof/gms_certified_props.mk
 # Keybox Overlay
 PRODUCT_PACKAGES += \
     CertifiedKeyboxOverlay
+
+# Game props
+TARGET_PRODUCT_PROP += vendor/spoof/gameprops/gameprops.prop

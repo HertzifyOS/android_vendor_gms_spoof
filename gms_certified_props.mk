be7f486c6ac1aeb4263d1e18e575ac4346c7304a
Joey <joey@evolution-x.org>
2025-10-16 08:45:11 +0900

    Update pihooks fp

--- gms_certified_props.mk
+++ gms_certified_props.mk
@@ -2,8 +2,8 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=mustang_beta \
-    persist.sys.pihooks_DEVICE?=mustang \
+    persist.sys.pihooks_PRODUCT?=comet_beta \
+    persist.sys.pihooks_DEVICE?=comet \
     persist.sys.pihooks_ID?=BP41.250916.009.A1 \
     persist.sys.pihooks_RELEASE?=12 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-10-05 \
@@ -11,6 +11,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=google/mustang_beta/mustang:16/BP41.250916.009.A1/14246511:user/release-keys \
-    PihooksGmsFp="google/mustang_beta/mustang:16/BP41.250916.009.A1/14246511:user/release-keys" \
-    PihooksGmsModel="Pixel 10 Pro XL"
+    BuildFingerprint=google/comet_beta/comet:16/BP41.250916.009.A1/14246511:user/release-keys \
+    PihooksGmsFp="google/comet_beta/comet:16/BP41.250916.009.A1/14246511:user/release-keys" \
+    PihooksGmsModel="Pixel 9 Pro Fold"
